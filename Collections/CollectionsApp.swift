26f124d4549f4864221f84b11ad4c9d66440e4f9
szholbarys <sakenzholbarys@gmail.com>
2024-09-20 12:01:03 +0500

    Initial Commit

---
+++ Collections/CollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionsApp.swift
+//  Collections
+//
+//  Created by Zholbarys on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
